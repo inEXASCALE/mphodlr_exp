22cc08fa195a9720929f18091589c6b8a2a740b9
chenxinye <cclcquant@yahoo.com>
2024-08-03 17:25:26 +0200

    add index

--- bin/bin_kernel1.m
+++
@@ -1,4 +0,0 @@
-function y = kernel1(x, y) % for scalar
-    y = repmat(x, size(x, 2), 1)' - repmat(y, size(y, 2), 1); 
-    y =  1 ./ y;
-end
--- bin/bin_kernel2.m
+++
@@ -1,3 +0,0 @@
-function y = kernel2(x, y) % for scalar
-    y = log(abs(repmat(x, size(x, 2), 1)' - repmat(y, size(y, 2), 1))); 
-end

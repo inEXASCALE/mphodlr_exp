106b558764aaa4055080b1a0f8d58607202a1e2c
the-null <xinyechenai@gmail.com>
2024-11-28 20:36:59 +0100

    Create schur_factors.m

---
+++ schur_factors.m
@@ -0,0 +1,9 @@
+function SF = schur_factors(M)
+    [m, n] = size(M);
+    m_half = ceil(m / 2);
+    n_half = ceil(n / 2);
+    
+    SF = zeros(m-m_half, n-n_half);
+    A = M(1:m_half, 1:n_half);
+    SF = M(m_half+1:end, n_half+1:end) - M((m_half+1):end, 1:n_half) * inv(A) * M(1:m_half, (n_half+1):end);
+end

d4ff3cad2ec431ed2bee61ea769c3990914bf2a5
the-null <xinyechenai@gmail.com>
2024-11-28 21:07:52 +0100

    Update exp_lu.m

--- exp_lu.m
+++ exp_lu.m
@@ -4,7 +4,7 @@ function exp_lu()
     clear all
     
     A = load('data/3-5000/root_P64_cs128.mat');
-    A =  A.A;
+    A =  schur_factors(A.A);
     
     u1 = precision('d');
     u2 = precision('s');

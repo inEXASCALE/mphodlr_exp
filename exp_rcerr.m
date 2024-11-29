3998102d7b2353251e02cba3f3664eb9e4ed5bab
the-null <xinyechenai@gmail.com>
2024-11-29 16:35:42 +0100

    Update exp_rcerr.m

--- exp_rcerr.m
+++ exp_rcerr.m
@@ -12,7 +12,7 @@ function exp_rcerr()
     
     u_chain = prec_chain(u1, u2, u3, u4, u5);
     
-    vareps = [10e-9, 10e-08, 10e-07, 10e-06, 10e-5, 10e-04, 10e-03, 10e-02]; % u2.u, u3.u
+    vareps = [10e-9, 10e-07, 10e-5, 10e-03, 10e-01]; % u2.u, u3.u
     depths = [2,  5,  8];
     
     n_d = size(depths, 2);
@@ -63,7 +63,7 @@ function exp_rcerr()
     
     u_chain = prec_chain(u1, u2, u3, u4, u5);
     
-    vareps = [10e-9, 10e-08, 10e-07, 10e-06, 10e-5, 10e-04, 10e-03, 10e-02]; % u2.u, u3.u
+    vareps = [10e-9, 10e-07, 10e-5, 10e-03, 10e-01]; % u2.u, u3.u
     depths = [2,  5,  8];
     
     n_d = size(depths, 2);
@@ -116,7 +116,7 @@ function exp_rcerr()
     
     u_chain = prec_chain(u1, u2, u3, u4, u5);
     
-    vareps = [10e-9, 10e-08, 10e-07, 10e-06, 10e-5, 10e-04, 10e-03, 10e-02]; % u2.u, u3.u
+    vareps = [10e-9, 10e-07, 10e-5, 10e-03, 10e-01]; % u2.u, u3.u
     depths = [2,  5,  8];
     
     n_d = size(depths, 2);

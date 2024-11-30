78bb3e25cd3bb23bfe28e7664c419c113e5a570b
the-null <xinyechenai@gmail.com>
2024-11-30 12:57:46 +0100

    Update plot_exp_storage.m

--- plot_exp_storage.m
+++ plot_exp_storage.m
@@ -1,5 +1,5 @@
 function plot_exp_storage()
-    vareps = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]; % u2.u, u3.u
+    vareps = [1e-7, 1e-4, 1e-1]; % u2.u, u3.u
     depths = [5, 8];
     
     load("results/storage_eff1.mat");

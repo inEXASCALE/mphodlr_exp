75bd7fac980f7c2c473d069b2d478bd713a75103
the-null <xinyechenai@gmail.com>
2025-12-17 16:50:55 +0100

    Fix Schur factor calculation in schur_factors.m

--- schur_factors.m
+++ schur_factors.m
@@ -3,6 +3,6 @@ function SF = schur_factors(M)
     m_half = ceil(m / 2);
     n_half = ceil(n / 2);
     
-    A = M(1:m_half, 1:n_half);
-    SF = M(m_half+1:end, n_half+1:end) - M((m_half+1):end, 1:n_half) / A * M(1:m_half, (n_half+1):end);
+    SF = M(1:m_half, 1:n_half);
+    SF = M(m_half+1:end, n_half+1:end) - M((m_half+1):end, 1:n_half) / SF * M(1:m_half, (n_half+1):end);
 end
